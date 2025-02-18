d978cbb10d8ad2520b16a89687d66e84b66c3ada
freak3123 <snehashradha1234@gmaul.com>
2025-02-19 02:26:50 +0530

    left nav bar skele

---
+++ Data/index.tsx
@@ -0,0 +1,39 @@
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+
+// Menu items.
+export const side_fea = [
+    {
+      title: "Home",
+      url: "#",
+      icon: Home,
+    },
+    {
+      title: "Discover",
+      url: "#",
+      icon: Inbox,
+    },
+    {
+      title: "Collection",
+      url: "#",
+      icon: Calendar,
+    },
+  ]
+
+  export const side_library = [
+    {
+      title: "Download",
+      url: "#",
+      icon: Search,
+    },
+    {
+      title: "Favorites",
+      url: "#",
+      icon: Settings,
+    },
+    {
+      title: "Local Files",
+      url: "#",
+      icon: Calendar,
+    },
+]
+  
